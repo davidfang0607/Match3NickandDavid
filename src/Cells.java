5a8ee060a6e0a1e18b106bbbd65db858e282bfb0
david_fang <david_fang@student.wayland.k12.ma.us>
2019-09-09 14:30:29 -0400

    david update

--- src/Cells.java
+++ src/Cells.java
@@ -1,21 +1,33 @@
 public class Cells {
-    private boolean exist;
+    //private boolean exist;
     private int cellValue;
     private int[][] cellValues;
 
     public Cells(){
-        exist = true;
+        //exist = true;
         cellValues = new int[10][10];
+
     }
 
+    //put random values in cellValues
     private int[][] randomizeBoard(){
-//        cellvint[10][10]
         for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
-
+                cellValue = (int)(Math.random()*3);
+                cellValues[r][c] = cellValue;
             }
         }
-
-        return new int[3][4];
+        return cellValues;
     }
+
+    private boolean check3InRow(){
+        for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 10; c++) {
+                if cellValues[r][c]
+            }
+        }
+    }
+
+
+
 }
