1657a91f5b35739cc9c022b726fa13a5974bebb0
sonic-nick <skypotatoe22@gmail.com>
2019-09-09 14:51:33 -0400

    push test - nick

--- src/Blocks.java
+++ src/Blocks.java
@@ -1,12 +1,11 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class Blocks {
     private int x;
     private int y;
+    private int[][] stuff;
     public Blocks(ArrayList[][] meh){
 
     }
 
-    public void checkMatch3(){
-
-    }
 }
